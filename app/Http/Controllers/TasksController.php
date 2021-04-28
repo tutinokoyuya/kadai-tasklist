de98077b4048de1b915b5e9b51835a06d04126bc
yuya.tutinoko <oyakoko2021@gmail.com>
2021-04-28 12:49:51 +0000

    kadai-colmn

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -44,7 +44,13 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required',
+        ]);
+        
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
@@ -90,9 +96,15 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'status' => 'required|max:10',
+            'content' => 'required',
+        ]);
+        
         //idの値でメッセージを検索して取得
         $task = Task::findOrFail($id);
         //メッセージを更新
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
         
